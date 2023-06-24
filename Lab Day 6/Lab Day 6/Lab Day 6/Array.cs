05d6af8ba1a468ca6774e780d473826bc7a7f156
Nipun Malinga <nipunmalinga11@gmail.com>
2023-06-24 12:30:18 +0530

    Q6 Done

---
+++ Lab Day 6/Lab Day 6/Lab Day 6/Array.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_Day_6
+{
+    internal class Array
+    {
+        private int Size;
+
+        public Array(int size)
+        {
+            this.Size = size;
+        }
+        public void setArray()
+        {
+            int[] arr = new int[Size];
+
+            for(int i = 0; i < Size; i++)
+            {
+                Console.Write($"Enter Value for index {i}: ");
+                arr[i] = int.Parse( Console.ReadLine());
+            }
+
+            Console.WriteLine("\nDisplaying The Array: \n");
+
+            for (int i = 0; i < Size; i++)
+            {
+                Console.Write($"{arr[i]} ");
+            }
+        }
+             
+    }
+}
---
+++ Lab Day 6/Lab Day 6/Lab Day 6/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_Day_6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter Array Size: ");
+            int size = int.Parse(Console.ReadLine());
+            
+            var Array = new Array(size);
+            Array.setArray();
+            Console.ReadLine();
+        }
+    }
+}
